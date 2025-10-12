85a814376aa9c34fc4d17cc410a999e92307de70
Tristan Gingold <tgingold@free.fr>
2025-10-12 12:44:42 +0200

    01-blink-pll: rework

--- 01-blink-pll/src/blink.vhd
+++ 01-blink-pll/src/blink.vhd
@@ -3,82 +3,79 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity blink is
-	port (
-		clk : in std_logic;
-		rst : in std_logic;
-		led : out std_logic;
-                dbg_tx: out std_logic;
-                dbg_rx: in std_logic
-	);
+  port (
+    clk : in std_logic;
+    rst : in std_logic;
+    led : out std_logic
+    );
 end entity;
 
 architecture rtl of blink is
+  component CC_PLL is
+    generic (
+      REF_CLK         : string;  -- reference input in MHz
+      OUT_CLK         : string;  -- pll output frequency in MHz
+      PERF_MD         : string;  -- LOWPOWER, ECONOMY, SPEED
+      LOW_JITTER      : integer; -- 0: disable, 1: enable low jitter mode
+      CI_FILTER_CONST : integer; -- optional CI filter constant
+      CP_FILTER_CONST : integer  -- optional CP filter constant
+      );
+    port (
+      CLK_REF             : in  std_logic;
+      USR_CLK_REF         : in  std_logic;
+      CLK_FEEDBACK        : in  std_logic;
+      USR_LOCKED_STDY_RST : in  std_logic;
+      USR_PLL_LOCKED_STDY : out std_logic;
+      USR_PLL_LOCKED      : out std_logic;
+      CLK0                : out std_logic;
+      CLK90               : out std_logic;
+      CLK180              : out std_logic;
+      CLK270              : out std_logic;
+      CLK_REF_OUT         : out std_logic
+      );
+  end component;
 
-	component CC_PLL is
-	generic (
-		REF_CLK         : string;  -- reference input in MHz
-		OUT_CLK         : string;  -- pll output frequency in MHz
-		PERF_MD         : string;  -- LOWPOWER, ECONOMY, SPEED
-		LOW_JITTER      : integer; -- 0: disable, 1: enable low jitter mode
-		CI_FILTER_CONST : integer; -- optional CI filter constant
-		CP_FILTER_CONST : integer  -- optional CP filter constant
-	);
-	port (
-		CLK_REF             : in  std_logic;
-		USR_CLK_REF         : in  std_logic;
-		CLK_FEEDBACK        : in  std_logic;
-		USR_LOCKED_STDY_RST : in  std_logic;
-		USR_PLL_LOCKED_STDY : out std_logic;
-		USR_PLL_LOCKED      : out std_logic;
-		CLK0                : out std_logic;
-		CLK90               : out std_logic;
-		CLK180              : out std_logic;
-		CLK270              : out std_logic;
-		CLK_REF_OUT         : out std_logic
-	);
-	end component;
-
-	signal clk0    : std_logic;
-	signal counter : unsigned(26 downto 0);
-
+  signal clk_100    : std_logic;
+  signal counter : unsigned(27 downto 0);
+  constant c_period : unsigned(counter'range) :=
+    to_unsigned(25_000_000 - 1, counter'length);
+  signal ledo : std_logic := '0';
+  signal pll_locked : std_logic;
 begin
-	socket_pll : CC_PLL
-	generic map (
-		REF_CLK         => "10.0",
-		OUT_CLK         => "100.0",
-		PERF_MD         => "ECONOMY",
-		LOW_JITTER      => 1,
-		CI_FILTER_CONST => 2,
-		CP_FILTER_CONST => 4
-	)
-	port map (
-		CLK_REF             => clk,
-		USR_CLK_REF         => '0',
-		CLK_FEEDBACK        => '0',
-		USR_LOCKED_STDY_RST => '0',
-		USR_PLL_LOCKED_STDY => open,
-		USR_PLL_LOCKED      => open,
-		CLK0                => clk0,
-		CLK90               => open,
-		CLK180              => open,
-		CLK270              => open,
-		CLK_REF_OUT         => open
-	);
+  inst_pll : CC_PLL
+    generic map (
+      REF_CLK         => "10.0",
+      OUT_CLK         => "50.0",
+      PERF_MD         => "ECONOMY",
+      LOW_JITTER      => 1,
+      CI_FILTER_CONST => 2,
+      CP_FILTER_CONST => 4
+      )
+    port map (
+      CLK_REF             => clk,
+      USR_CLK_REF         => '0',
+      CLK_FEEDBACK        => '0',
+      USR_LOCKED_STDY_RST => '0',
+      USR_PLL_LOCKED_STDY => open,
+      USR_PLL_LOCKED      => pll_locked,
+      CLK0                => clk_100,
+      CLK90               => open,
+      CLK180              => open,
+      CLK270              => open,
+      CLK_REF_OUT         => open
+      );
 
-	process(clk0)
-	begin
-		if rising_edge(clk0) then
-			if rst = '0' then
-				counter <= (others => '0');
-			else
-				counter <= counter + 1;
-			end if;
-		end if;
-	end process;
+  process(clk_100)
+  begin
+    if rising_edge(clk_100) then
+      if rst = '0' or pll_locked = '0' or counter = 0 then
+        counter <= c_period - 1;
+        ledo <= not ledo;
+      else
+        counter <= counter - 1;
+      end if;
+    end if;
+  end process;
 
-	led <= counter(26);
-
-        dbg_tx <= dbg_rx;
-
---        led <= rst;
+  led <= ledo;
 end architecture;
--- README.md
+++ README.md
@@ -23,3 +23,23 @@ Not required for the demos, but useful.
 * [GateMate A1 datasheet](https://colognechip.com/docs/ds1001-gatemate1-datasheet-latest.pdf)
 * [GateMate A1 Pin Lists](https://www.colognechip.com/docs/ds1001-gatemate1-attachment-latest.zip)
 * [GateMate A1 library](https://www.colognechip.com/docs/ug1001-gatemate1-primitives-library-latest.pdf)
+
+## Examples
+
+### 00-blink
+
+A very simple and standalone example.  Useful to test your
+installation.  If `make load` fails with a message such as `cannot
+find board`, it might be because you need to be root to access to the
+USB port.  However, if you simply try `sudo openFPGALoader ...`, it
+would probably fail as `PATH` is not preserved (or overwritten) and
+thus `openFPGALoader` would not be found.  You need to do: `sudo
+/<extracted_location>/oss-cad-suite/bin/openFPGALoader ...`
+
+### 01-blink-pll
+
+A very simple and standalone example which instantiate the PLL.  This
+hard block is probably the main one you would instantiate in most of
+your designs.  The PLL compoment is declared within the top-level.
+
+Also, the PLL locked output is used as a reset signal.
