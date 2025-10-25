9a25a63e799426840df5cad011e1feb2b52d8c0d
Tristan Gingold <tgingold@free.fr>
2025-10-25 17:03:44 +0200

    08-ps2-print: renaming

--- 08-ps2-print/src/ps2_print.vhdl
+++ 08-ps2-print/src/ps2_print.vhdl
@@ -18,21 +18,22 @@ entity ps2_print is
 end entity;
 
 architecture rtl of ps2_print is
+  constant c_clk_freq : natural := 50_000_000;
+
   signal clk_50m : std_logic;
   signal rst_n   : std_logic;
-  signal counter : unsigned(26 downto 0);
 
-  signal rx_byte : std_logic_vector(8 downto 0);
-  signal rx_ready, rx_done : std_logic;
-  signal rx_cnt : natural range 0 to 9;
+  signal ps2_rx_byte : std_logic_vector(8 downto 0);
+  signal ps2_rx_ready, ps2_rx_done : std_logic;
+  signal ps2_rx_cnt : natural range 0 to 9;
   
   signal led_count : natural range 0 to 5_000_000 := 0;
 
-  signal tx_byte, tx_dir : std_logic_vector(7 downto 0);
-  signal tx_hex : std_logic_vector(3 downto 0);
-  type t_tx_state is (S_IDLE, S_HIGH, S_LOW, S_SPACE);
-  signal tx_state : t_tx_state;
-  signal tx_stb, tx_stb_int, tx_done : std_logic;
+  signal uart_tx_byte, uart_tx_dir : std_logic_vector(7 downto 0);
+  signal uart_tx_hex : std_logic_vector(3 downto 0);
+  type t_uart_tx_state is (S_IDLE, S_HIGH, S_LOW, S_SPACE);
+  signal uart_tx_state : t_uart_tx_state;
+  signal uart_tx_stb, uart_tx_stb_int, uart_tx_done : std_logic;
 
   signal ps2_clk, ps2_clk_d : std_logic;
 begin
@@ -45,16 +46,16 @@ begin
 
    inst_uart: entity work.uart
      generic map (
-       g_clk_freq => 50_000_000,
+       g_clk_freq => c_clk_freq,
        g_baudrate => 115200)
      port map (
        clk_i      => clk_50m,
        rst_n_i    => rst_n,
        tx_o       => tx_o,
        rx_i       => rx_i,
-       tx_byte_i  => tx_byte,
-       tx_stb_i   => tx_stb,
-       tx_done_o  => tx_done,
+       tx_byte_i  => uart_tx_byte,
+       tx_stb_i   => uart_tx_stb,
+       tx_done_o  => uart_tx_done,
        rx_byte_o  => open,
        rx_stb_o   => open);
 
@@ -73,25 +74,25 @@ begin
   proc_rx: process(clk_50m)
   begin
     if rising_edge(clk_50m) then
-      rx_done <= '0';
+      ps2_rx_done <= '0';
       
       if rst_n = '0' then
-        rx_ready <= '1';
+        ps2_rx_ready <= '1';
       else
-        if ps2_clk_d = '0' and ps2_clk = '1' then
-          --  Rising edge on the clock.
-          if rx_ready = '1' and ps2_data_b = '0' then
+        if (ps2_clk_d = '0' and ps2_clk = '1') then
+          --  Rising edge on the clock when not transmitting.
+          if ps2_rx_ready = '1' and ps2_data_b = '0' then
             --  Start
-            rx_cnt <= 0;
-            rx_ready <= '0';
-          elsif rx_ready = '0' then
-            if rx_cnt = 9 then
+            ps2_rx_cnt <= 0;
+            ps2_rx_ready <= '0';
+          elsif ps2_rx_ready = '0' then
+            if ps2_rx_cnt = 9 then
               --  Stop
-              rx_done <= '1';
-              rx_ready <= '1';
+              ps2_rx_done <= '1';
+              ps2_rx_ready <= '1';
             else
-              rx_byte <= ps2_data_b & rx_byte(8 downto 1);
-              rx_cnt <= rx_cnt + 1;
+              ps2_rx_byte <= ps2_data_b & ps2_rx_byte(8 downto 1);
+              ps2_rx_cnt <= ps2_rx_cnt + 1;
             end if;
           end if;
         end if;
@@ -99,6 +100,7 @@ begin
     end if;
   end process;
 
+  --  LED control.
   led_o <= '1' when led_count = 0 else '0';
 
   process (clk_50m)
@@ -107,7 +109,7 @@ begin
       if led_count /= 0 then
         led_count <= led_count - 1;
       end if;
-      if rx_done = '1' then
+      if ps2_rx_done = '1' then
         led_count <= 5_000_000;
       end if;
     end if;
@@ -117,34 +119,34 @@ begin
   process (clk_50m)
   begin
     if rising_edge(clk_50m) then
-      tx_stb_int <= '0';
+      uart_tx_stb_int <= '0';
       if rst_n = '0' then
-        tx_dir <= x"00";
-        tx_state <= S_IDLE;
+        uart_tx_dir <= x"00";
+        uart_tx_state <= S_IDLE;
       else
-        case tx_state is
+        case uart_tx_state is
           when S_IDLE =>
-            if rx_done = '1' then
-              tx_hex <= rx_byte(7 downto 4);
-              tx_stb_int <= '1';
-              tx_state <= S_HIGH;
+            if ps2_rx_done = '1' then
+              uart_tx_hex <= ps2_rx_byte(7 downto 4);
+              uart_tx_stb_int <= '1';
+              uart_tx_state <= S_HIGH;
             end if;
           when S_HIGH =>
-            if tx_done = '1' then
-              tx_hex <= rx_byte(3 downto 0);
-              tx_stb_int <= '1';
-              tx_state <= S_LOW;
+            if uart_tx_done = '1' then
+              uart_tx_hex <= ps2_rx_byte(3 downto 0);
+              uart_tx_stb_int <= '1';
+              uart_tx_state <= S_LOW;
             end if;
           when S_LOW =>
-            if tx_done = '1' then
-              tx_dir <= x"20";
-              tx_stb_int <= '1';
-              tx_state <= S_SPACE;
+            if uart_tx_done = '1' then
+              uart_tx_dir <= x"20";
+              uart_tx_stb_int <= '1';
+              uart_tx_state <= S_SPACE;
             end if;
           when S_SPACE =>
-            if tx_done = '1' then
-              tx_dir <= x"00";
-              tx_state <= S_IDLE;
+            if uart_tx_done = '1' then
+              uart_tx_dir <= x"00";
+              uart_tx_state <= S_IDLE;
             end if;
         end case;
       end if;
@@ -155,20 +157,20 @@ begin
   process (clk_50m)
   begin
     if rising_edge(clk_50m) then
-      tx_stb <= '0';
-      if rst_n = '1' and tx_stb_int = '1' then
-        if tx_dir /= x"00" then
-          tx_byte <= tx_dir;
+      uart_tx_stb <= '0';
+      if rst_n = '1' and uart_tx_stb_int = '1' then
+        if uart_tx_dir /= x"00" then
+          uart_tx_byte <= uart_tx_dir;
         else
-          if unsigned(tx_hex) <= 9 then
-            tx_byte(7 downto 4) <= x"3";
-            tx_byte(3 downto 0) <= tx_hex;
+          if unsigned(uart_tx_hex) <= 9 then
+            uart_tx_byte(7 downto 4) <= x"3";
+            uart_tx_byte(3 downto 0) <= uart_tx_hex;
           else
-            tx_byte(7 downto 4) <= x"6";
-            tx_byte(3 downto 0) <= std_logic_vector(unsigned(tx_hex) - 9);
+            uart_tx_byte(7 downto 4) <= x"6";
+            uart_tx_byte(3 downto 0) <= std_logic_vector(unsigned(uart_tx_hex) - 9);
           end if;
         end if;
-        tx_stb <= '1';
+        uart_tx_stb <= '1';
       end if;
     end if;
   end process;
